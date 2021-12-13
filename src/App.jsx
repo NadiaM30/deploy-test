6af112000d97fe3951cb79bb3d926f55700b4271
Teuta <teutaberisha2022@gmail.com>
2021-12-13 21:17:56 +0300

    test - 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import lottie from "lottie-web";
+import * as lottie from 'lottie-web/build/player/lottie_light'
+//import lottie from "lottie-web";
 import lottieImage from "./assets/olympic.json";
 import lottieImage1 from "./assets/1.json";
 import { createEffect } from "solid-js/dist/server";
@@ -9,6 +10,7 @@ function App() {
   createEffect(() => {
     lottie.loadAnimation({
       container: document.querySelector("#lottie-image"),
+      path: lottieImage,
       animationData: lottieImage,
       renderer: "svg",
       loop: true,
@@ -16,11 +18,13 @@ function App() {
     });
     lottie.loadAnimation({
       container: document.querySelector("#lottie-image1"),
+      path: lottieImage1,
       animationData: lottieImage1,
       renderer: "svg",
       loop: true,
       autoplay: true,
     });
+    lottie.play();
   });
 
   return (
